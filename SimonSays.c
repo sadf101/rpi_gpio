82297785c037550c806a28e8d6ee9bbaa5fb1513
Samuel Rogers <srogers@dyn3194-57.wlan.ic.ac.uk>
2018-06-15 00:24:06 +0100

    done i think

--- SimonSays.c
+++ SimonSays.c
@@ -7,91 +7,65 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#define BCM2708_PERI_BASE        0x20000000
-#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+#define SEED_RAND srand(time(NULL))
+#define MAX_LEVELS 5
 
-#define PAGE_SIZE (4*1024)
-#define BLOCK_SIZE (4*1024)
+typedef enum button {B1 = 4, B2 = 25, B3 = 27} BUTTON;
+typedef enum led {L1 = 22, L2 = 23, L3 = 24} LED;
 
-int  mem_fd;
-void *gpio_map;
-volatile unsigned *gpio;
+volatile unsigned *virtualGpio;
 
-#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
-#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
-#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
-#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
-#define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
-#define GPIO_PULL *(gpio+37) // Pull up/pull down
-#define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
-
-void setup_io() {
-   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-      printf("can't open /dev/mem \n");
-      exit(-1);
-   }
-   gpio_map = mmap(
-      NULL,             //Any adddress in our space will do
-      BLOCK_SIZE,       //Map length
-      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-      MAP_SHARED,       //Shared with other processes
-      mem_fd,           //File to map
-      GPIO_BASE         //Offset to GPIO peripheral
-   );
-   close(mem_fd); //No need to keep mem_fd open after mmap
-   if (gpio_map == MAP_FAILED) {
-      printf("mmap error %d\n", (int)gpio_map);//errno also set!
-      exit(-1);
-   }
-   gpio = (volatile unsigned *)gpio_map;
+volatile unsigned *setupGpioMap() {
+  int memoryFile;
+  if ((memoryFile = open("/dev/mem", O_RDWR|O_SYNC)) < 0) {
+    return NULL;
+  }
+  #define PHYSICAL_GPIO 0x20200000
+  #define BLOCK_SIZE (4*1024)
+  void *gpioMap = mmap(
+    NULL,
+    BLOCK_SIZE,
+    PROT_READ|PROT_WRITE,
+    MAP_SHARED,
+    memoryFile,
+    PHYSICAL_GPIO
+  );
+  if (close(memoryFile) == -1) {
+    return NULL;
+  }
+  if (gpioMap == MAP_FAILED) {
+    return NULL;
+  }
+  return (volatile unsigned *)gpioMap;
 }
 
-void printButton(int g) {
-  if (GET_GPIO(g)) // !=0 <-> bit is 1 <- port is HIGH=3.3V
-    printf("Button pressed!\n");
-  else // port is LOW=0V
-    printf("Button released!\n");
+void inputPin(int pin) {
+  *(virtualGpio + (pin / 10)) &= ~(7 << ((pin % 10) * 3));
 }
 
-#define getRandomPin (rand() % 3)
-#define initRand srand(time(NULL))
+void outputPin(int pin) {
+  *(virtualGpio + (pin / 10)) |=  (1 << ((pin % 10) * 3));
+}
+
+bool getPin(int pin) {
+  return *(virtualGpio + 13) & (1 << pin);
+}
 
 void setPin(int pin) {
-  GPIO_SET = 1 << (pin + 22);
+  *(virtualGpio+7) = 1 << pin;
 }
 
 void clearPin(int pin) {
-  GPIO_CLR = 1 << (pin + 22);
+  *(virtualGpio+10) = 1 << pin;
 }
 
-void flashPin(int pin) {
-  printf("flashing %d\n", pin);
-  setPin(pin);
+void flashLed(LED led) {
+  setPin(led);
   sleep(1);
-  clearPin(pin);
-}
-
-void flashAll(void);
-
-void initPins(void) {
-  for (int i = 0; i < 3; i++) {
-    INP_GPIO(i + 22);
-    OUT_GPIO(i + 22);
-    flashPin(i);
-  }
-  flashAll();
-}
-
-void flashArray(int *array, int size) {
-  for (int i = 0; i < size; i++) {
-    flashPin(array[i]);
-    sleep(1);
-  }
+  clearPin(led);
 }
 
 void flashAll(void) {
-  printf("flash all\n");
   setPin(0);
   setPin(1);
   setPin(2);
@@ -101,23 +75,103 @@ void flashAll(void) {
   clearPin(2);
 }
 
+void initPins(void) {
+  inputPin(B1);
+  inputPin(B2);
+  inputPin(B3);
+  inputPin(L1);
+  inputPin(L2);
+  inputPin(L3);
+  outputPin(L1);
+  outputPin(L2);
+  outputPin(L3);
+  for (int i = 0; i < 3; i++) {
+    flashLed(i);
+  }
+  flashAll();
+}
+
+void flashArray(LED *array, int size) {
+  for (int i = 0; i < size; i++) {
+    flashLed(array[i] + 22);
+    sleep(1);
+  }
+}
+
+bool pollButton(BUTTON expected) {
+  bool one = false;
+  bool two = false;
+  bool three = false;
+  switch (expected) {
+    case B1:
+      one = true;
+      break;
+    case B2:
+      two = true;
+      break;
+    case B3:
+      three = true;
+      break;
+  }
+  do {
+    if (getPin(B1)) return one;
+    if (getPin(B2)) return two;
+    if (getPin(B3)) return three;
+  } while (true);
+}
+
+LED getRandomLed() {
+  switch (rand() % 3) {
+    case 1: return L1;
+    case 2: return L2;
+    case 3: return L3;
+    default: return -1;
+  }
+}
+
+BUTTON ledToButton(LED led) {
+  switch (led) {
+    case L1: return B1;
+    case L2: return B2;
+    case L3: return B3;
+    default: return -1;
+  }
+}
+
+bool checkOrder(LED *ledArray, int size) {
+  for (int i = 0; i < size; i++) {
+    if (!pollButton(ledToButton(ledArray[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main(int argc, char **argv)
 {
-  int score = 0;
-  int *array = NULL;
-  if (array = NULL) return -1;
-  setup_io();
-  initRand;
+  if ((virtualGpio = setupGpioMap()) == NULL) {
+    return -1;
+  }
+  SEED_RAND;
   initPins();
   sleep(1);
+  int score = 0;
+  LED *ledArray = NULL;
+  bool running;
   do {
-    array = realloc(array, (score + 1) * sizeof(int));
-    array[score] = getRandomPin;
-    flashArray(array, score + 1);
+    ledArray = realloc(ledArray, (score + 1) * sizeof(int));
+    if (ledArray == NULL) {
+      return -1;
+    }
+    ledArray[score] = getRandomLed();
+    flashArray(ledArray, score + 1);
+    if ((running = checkOrder(ledArray, score + 1))) {
+      score++;
+    }
+  } while (score < MAX_LEVELS && running);
+  for (int i = 0; i < score; i++) {
     flashAll();
-    sleep(1);
-    score++;
-  } while (score < 10);
-  free(array);
+  }
+  free(ledArray);
   return 0;
 }
