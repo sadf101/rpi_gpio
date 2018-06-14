80f49340b63bce7c8cd8b83f66263a23a6b901cc
Sam Rogers <sjr17@ic.ac.uk>
2018-06-14 16:56:26 +0100

    initial files added

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "RPI.h"
+
+int main(
+{
+  if(map_peripheral(&gpio) == -1)
+  {
+    printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
+    return -1;
+  }
+
+  // Define pin 7 as output
+  INP_GPIO(4);
+  OUT_GPIO(4);
+
+  while(1)
+  {
+    // Toggle pin 7 (blink a led!)
+    GPIO_SET = 1 << 4;
+    sleep(1);
+
+    GPIO_CLR = 1 << 4;
+    sleep(1);
+  }
+
+  return 0;
+}
